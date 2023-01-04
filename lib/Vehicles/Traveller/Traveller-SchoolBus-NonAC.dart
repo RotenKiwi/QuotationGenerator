18e669f39de6b58c4fed1e62e1a214099941b926
RotenKiwi <joshiparth212@gmail.com>
2023-01-04 15:02:54 +0530

    traveller type choose
    added prices and taxes for traveller schoolbus  non ac

---
+++ lib/Vehicles/Traveller/Traveller-SchoolBus-NonAC.dart
@@ -0,0 +1,37 @@
+class TravellerPassengerAC {
+  List<String> model = [
+    'T1 SCB 3350  17 Seater+Driver PS ABS FDSS Vinyl Non AC',
+    'T1 SCB 3700  17 Seater+Driver PS ABS FDSS Vinyl Non AC',
+    'T1 SCB 4020  20 Seater+Driver PS ABS FDSS Vinyl Non AC',
+  ];
+
+  List<int> price = [
+    1600347,
+    1720801,
+    1876548,
+  ];
+
+
+  List<int> rtoTaxSchool = [
+    100 * 17,
+    100 * 17,
+    100 * 20,
+  ];
+
+
+  // List<int> rtoTaxStaff = [
+  //   2000 * 13,
+  //   2000 * 17,
+  //   1000 * 20,
+  //   1000 * 22,
+  //   1000 * 25,
+  // ];
+
+  // List<int> rtoTaxTT = [
+  //   6500 * 14,
+  //   6500 * 17,
+  //   6500 * 20,
+  //   6500 * 22,
+  //   6500 * 25,
+  // ];
+}
